93a7359e4f2b35eb1a5be756597f3afc76e080e2
Joël Lamotte <mjklaim@gmail.com>
2010-06-08 07:01:09 +0000

    - WIP : adding guns...

--- RadiantLaserCross/RLC_Ship.h
+++ RadiantLaserCross/RLC_Ship.h
@@ -2,21 +2,30 @@
 #define RLC_SHIP_H
 #pragma once
 
+#include <array>
+
 #include "RLC_GameEntity.h"
+#include "RLC_Gun.h"
 
 namespace rlc
 {
 	/** No documentation yet.
 	*/
+	template< unsigned int MAX_GUN_COUNT >
 	class Ship
 		: public GameEntity
 	{
 	public:
 
+		Ship(){}
+		
+	protected:
 
+		// ? guns access? add/remove guns?
 		
 	private:
-	
+		
+		std::array< GunSlot, MAX_GUN_COUNT > m_guns;
 	};
 	
 }
