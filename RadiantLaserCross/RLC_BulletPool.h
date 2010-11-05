2e32e9982b1bffd522f318b973dcb9481fac7c3c
klaim <klaim@localhost>
2010-11-05 21:57:21 +0100

    Made GameEntitites non-copyable (entity semantic) and had to make the BulletManager have a statically built BulletPool.

---
+++ RadiantLaserCross/RLC_BulletPool.h
@@ -0,0 +1,34 @@
+#ifndef RLC_BULLETPOOL_H
+#define RLC_BULLETPOOL_H
+
+#include <array>
+#include <algorithm>
+#include <boost/noncopyable.hpp>
+
+namespace rlc
+{
+	template< std::size_t MAX_BULLETS >
+	class BulletPool
+		: boost::noncopyable
+	{
+	public:
+
+		Bullet* find_dead_bullet()
+		{
+			auto it = std::find_if( m_bullets.begin(), m_bullets.end(), []( Bullet& bullet )->bool { return !bullet.is_alive(); } );
+			if( it != m_bullets.end() )
+				return &(*it);
+			else
+				return nullptr;
+		}
+
+	private:
+
+		typedef std::array< Bullet, MAX_BULLETS > Bullets;
+		Bullets m_bullets;
+
+	};
+	
+}
+
+#endif 
